9b065d8e240816623d3a2c2a6cd85ffa4942a82b
Alan <alan.t.cumiskey@gmail.com>
2015-03-08 21:57:19 +0000

    any new stuff

--- app/assets/javascripts/redactor-rails/config.js
+++ app/assets/javascripts/redactor-rails/config.js
@@ -9,7 +9,7 @@ window.init_redactor = function(){
         // You can specify, which ones plugins you need.
         // If you want to use plugins, you have add plugins to your
         // application.js and application.css files and uncomment the line below:
-        "plugins": ['fontsize', 'fontcolor', 'fontfamily', 'fullscreen', 'textdirection', 'clips'],
+        //"plugins": ['fontsize', 'fontcolor', 'fontfamily', 'fullscreen', 'textdirection', 'clips'],
         "imageUpload":"/redactor_rails/pictures?" + params,
         "imageGetJson":"/redactor_rails/pictures",
         "fileUpload":"/redactor_rails/documents?" + params,
